cde2e069fb618bc2e9ed617da5a242861964edcb
Darragh ORiordan <darragh.oriordan@gmail.com>
2016-11-06 17:09:37 +1300

    clean up some methods add tests proj

---
+++ ResumeAspNet/Controllers/Api/ResumeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ResumeAspNet.Controllers.Api
+{
+    [Authorize]
+    public class ResumeController : ApiController
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            return "Darragh's resume" ;
+        }
+    }
+}
