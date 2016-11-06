21009dca4bc34498228df580d4bc37a8b34b3a06
Darragh ORiordan <darragh.oriordan@gmail.com>
2016-11-06 22:38:47 +1300

    add some model and set up nunit

--- ResumeAspNet/Controllers/Api/ResumeController.cs
+++ ResumeAspNet/Controllers/Api/ResumeController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace ResumeAspNet.Controllers.Api
 {
-    [Authorize]
+   
     public class ResumeController : ApiController
     {
-        // GET api/<controller>
-        public string Get()
+        // yea yea not at all async yet!
+        public async Task<string> Get()
         {
-            return "Darragh's resume" ;
+            return "Darragh's resume";
         }
     }
 }
---
+++ Tests/ResumeServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+namespace Tests
+{
+    [TestFixture]
+    public class ResumeServiceTests
+    {
+        [Test]
+        public void can_get_a_resume()
+        {
+            var resumeService = new ResumeAspNet.Services.ResumeService();
+            var result = resumeService.GetResume();
+
+            result.Should().NotBeNull();
+            result.ResumeContents.Should().NotBeEmpty();
+        }
+    }
+}
