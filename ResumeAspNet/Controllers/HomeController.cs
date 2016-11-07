b5a6212eff68c903d9441ca62d45d74c5676c620
Darragh ORiordan <darragh.oriordan@gmail.com>
2016-11-07 23:05:25 +1300

    add google auth

--- ResumeAspNet/Controllers/HomeController.cs
+++ ResumeAspNet/Controllers/HomeController.cs
@@ -2,12 +2,15 @@
 
 namespace ResumeAspNet.Controllers
 {
+    
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            var service = new ResumeAspNet.Services.ResumeService();
+
             ViewBag.Title = "Darragh's Resume";
-            return View();
+            return View(service.GetResume());
         }
     }
 }
