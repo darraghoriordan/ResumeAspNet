54f31661a095c0c08bae621f5ef7cfb7d6df70f1
Darragh ORiordan <darragh.oriordan@gmail.com>
2016-12-11 19:22:07 +1300

    make the menus look the same

--- ResumeAspNet/App_Start/BundleConfig.cs
+++ ResumeAspNet/App_Start/BundleConfig.cs
@@ -19,13 +19,15 @@ namespace Fuzzyminds.ResumeAspNet
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                    //  "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                     // "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/wordpressthemecss.css"));
+                     
+                   //   "~/Content/bootstrap.css",
+                       "~/Content/wordpressthemecss.css",
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
