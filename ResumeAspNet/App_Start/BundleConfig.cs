505c67dd126d93832b92740d75ccfc3d090add8c
Darragh ORiordan <darragh.oriordan@gmail.com>
2016-12-11 19:02:01 +1300

    add wp css

--- ResumeAspNet/App_Start/BundleConfig.cs
+++ ResumeAspNet/App_Start/BundleConfig.cs
@@ -19,12 +19,13 @@ namespace Fuzzyminds.ResumeAspNet
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                    //  "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                     // "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/wordpressthemecss.css"));
         }
     }
 }
